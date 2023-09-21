6c116051b8b05d6f2fb2d707eb8b4464671c6436
Demend2607 <finder2607011@gmail.com>
2023-09-21 14:11:03 +1000

    drop box full complete

--- src/components/cart-dropdown/CartDropDown.component.jsx
+++ src/components/cart-dropdown/CartDropDown.component.jsx
@@ -1,11 +1,26 @@
+import { useContext } from 'react';
+import { CartContext } from '../../context/cart.context';
+
 import Button from '../button/Button.component';
+
+import CartItem from '../cart-item/CartItem.component';
 
 import './cartDropDown.styles.scss';
 
 const CartDropDown = () => {
+	const { cartItems } = useContext(CartContext);
 	return (
 		<div className="cart-dropdown-container">
-			<div className="cart-items" />
+			{cartItems.length === 0 ? (
+				<span className="empty-message">Card list is empty</span>
+			) : (
+				<div className={`cart-items ${cartItems.length >= 4 ? 'cart-overflow' : ''}   `}>
+					{cartItems.map((item) => (
+						<CartItem key={item.id} cartItem={item} />
+					))}
+				</div>
+			)}
+
 			<Button>Go to checkout</Button>
 		</div>
 	);
